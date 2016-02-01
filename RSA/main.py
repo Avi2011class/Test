e0c17a204808da00646366d748faf2ba1d83fa31
Alexander Ivanov <avi2011class@yandex.ru>
2016-02-01 21:21:53 +0300

    Update main.py

--- RSA/main.py
+++ RSA/main.py
@@ -1,13 +1,16 @@
 import re
-import clipboard
-from coder import Coder
-from keygen import Key
+import tkinter
+from coder import StringCoder
+from keygen import Key, CreateKeys
 
-# Корректные ключи по-умолчанию
-Key1 = Key(257, 146969)
-Key2 = Key(59921, 146969)
-Coder1 = Coder(Key1)
-Coder2 = Coder(Key2)
+tk = tkinter.Tk()
+tk.withdraw()
+
+# Некорректные ключи по-умолчанию
+Key1 = Key.ReadFromFile('OpenKey')
+Key2 = Key.ReadFromFile('ClosedKey')
+Coder1 = StringCoder(Key1)
+Coder2 = StringCoder(Key2)
 
 while True:
 	print('''Выберите действие:
@@ -15,28 +18,39 @@ while True:
 	2. Установить файл ключа для расшифровки
 	3. Зашифровать строку
 	4. Расшифровать строку
-	5. Выйти
+	5. Создать новые ключи
+	6. Выйти
 >>''', end = '')
 	command = int(input())
 	if command == 1:
 		filename = input('Имя файла: ')
 		Key1 = Key.ReadFromFile(filename)
-		Coder1 = Coder(Key1)
+		Coder1 = StringCoder(Key1)
 	elif command == 2:
 		filename = input('Имя файла: ')
 		Key2 = Key.ReadFromFile(filename)
-		Coder2 = Coder(Key2)
+		Coder2 = StringCoder(Key2)
 	elif command == 3:
-		string = input("Введите строку для шифрования: ")
+		string = input("Введите строку для шифрования: ").replace('\n', '')
 		result = Coder1.CodeString(string)
-		clipboard.Set(result)
+		tk.clipboard_clear()
+		tk.clipboard_append(result)
 		print('Зашифрованная срока(скопирована в буфер обмена):', result)
 	elif command == 4:
 		string = input("Введите строку для дешифрования: ")
-		result = Coder2.CodeString(string)
-		clipboard.Set(result)
-		print('Дешафрованная срока(скопирована в буфер обмена):', result)
+		result = Coder2.CodeIntString(string)
+		tk.clipboard_clear()
+		tk.clipboard_append(result)
+		print('Дешифрованная строка(скопирована в буфер обмена):', result)
 	elif command == 5:
+		KeyLength = int(input('Длина ключей (8-50 бит): '))
+		CreateKeys(KeyLength, KeyLength + 1, "OpenKey", "ClosedKey")
+		Key1 = Key.ReadFromFile('OpenKey')
+		Key2 = Key.ReadFromFile('ClosedKey')
+		Coder1 = StringCoder(Key1)
+		Coder2 = StringCoder(Key2)
+		print('Новые ключи записаны в файлы OpenKey и  ClosedKey, данные в памяти обновлены')
+	elif command == 6:
 		exit(0)
 	else:
 		print('Попробуем еще разок')
